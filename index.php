dc337615be50ac4247e596bc925342fef908fc7e
Vasmayer <lealimarco90@gmail.com>
2022-03-03 16:53:01 +0100

    added product

--- index.php
+++ index.php
@@ -9,10 +9,20 @@ Il pagamento avviene con la carta di credito, che non deve essere scaduta. */
 
 require_once 'classes/Customer.php';
 require_once 'classes/CreditCard.php'; 
+require_once 'classes/Product.php'; 
 
 $customer = new Customer('Marco','Leali',true);
 $credit_card = new CreditCard('1234-1234-1234-1234',$customer->getNameComplete(),'04/02/2022','000');
 $customer->setCreditCard($credit_card);
 var_dump($customer);
 
+$cuccia = new Product('Cuccia',25,'Cuccia per cani e gatti 25X25','Accessori',4.1);
+$crocchette = new Product('Crocchette',9.80,'Gustose crocchette per il tuo cane','Alimentazione',4.5);
+$palla = new Product('Palla',9.80,'Pallina in lana per divertire il tuo gatto','Giochi',4.9);
+$shampoo = new Product('Shampoo',7.30,'Uno shampoo delicato per far splendere il pelo del tuo cane','Accessori',4.3);
+
+var_dump($shampoo);
+var_dump($palla);
+var_dump($crocchette);
+var_dump($cuccia);
 ?>
